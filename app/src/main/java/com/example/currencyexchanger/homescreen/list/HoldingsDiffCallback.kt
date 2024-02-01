ed8a52e2b7bbad7e28b1e4ded2aa028c9b934cd4
DGojka <dawidgojka4@gmail.com>
2024-02-02 00:37:31 +0100

    holdings recyclerview

---
+++ app/src/main/java/com/example/currencyexchanger/homescreen/list/HoldingsDiffCallback.kt
@@ -0,0 +1,13 @@
+package com.example.currencyexchanger.homescreen.list
+
+import androidx.recyclerview.widget.DiffUtil
+
+class HoldingsDiffCallback : DiffUtil.ItemCallback<String>() {
+    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem.hashCode() == newItem.hashCode()
+    }
+
+    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+        return oldItem == newItem
+    }
+}
